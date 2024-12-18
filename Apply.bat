dc90117b3782cc2a03a70d298bf33397cca98c6a
Himanshu8055 <58818455+Himanshu8055@users.noreply.github.com>
2024-12-18 08:15:35 +0530

    Initial commit: Added all files

---
+++ Apply.bat
@@ -0,0 +1,6 @@
+@echo off
+start microsoft-edge:https://internshala.com/student/dashboard
+start microsoft-edge:https://in.indeed.com/?from=gnav-homepage
+start microsoft-edge:https://www.linkedin.com/jobs/
+start microsoft-edge:https://www.naukri.com/mnjuser/homepage
+start microsoft-edge:https://cutshort.io/profile/all-jobs?matchesfor=67622d67fe549400286e0df2
